f6a6d6f6bf42d355e6d9f1d3f1d97c11cb7dd34b
Parth Patel <33936278+ParthiOS@users.noreply.github.com>
2023-10-27 17:13:58 -0400

    Add files via upload
    Just updating the file

--- weatherapi.py
+++ weatherapi.py
@@ -8,6 +8,9 @@ url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
 
 response = requests.get(url)
 
+
+# Add description here as well as a query for the forecast
+## eventually create a graphical interface that shows all data on the same screen
 if response.status_code == 200:
     dat = response.json()
     temp = dat['main']['temp']
